670b55ada7091e2f16b4d9da13feef38cf558787
redrian20 <109889533+redrian20@users.noreply.github.com>
2022-07-24 14:17:01 +0700

    menambahkan file aritmatika

---
+++ Aritmatika.php
@@ -0,0 +1,22 @@
+
+
+<?php
+//penjumlahan
+//  echo(10+2);
+
+//pengurangan
+//  echo(10-2);
+
+//perkalian
+//    echo(10*2);
+
+//pembagian
+//    echo(10/2);
+
+$umurSaya = 19;
+$umurImeh = 8;
+$umurKalyn = 16;
+
+echo($umurSaya - $umurKalyn + $umurImeh);
+
+?>
