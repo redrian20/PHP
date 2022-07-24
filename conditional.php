fd3950bcdc6ecfb1c3fb77a240d0c422c96dbf5f
redrian20 <109889533+redrian20@users.noreply.github.com>
2022-07-24 14:19:19 +0700

    menambahkan file conditional

---
+++ conditional.php
@@ -0,0 +1,13 @@
+<?php
+
+    $usiaMobil = 10;
+
+    //jika umur mobil < 10 maka tidak perlu di ganti
+    //jika umur mobil > 10 maka perlu di ganti
+    if($usiaMobil >= 10) {
+        echo 'Perlu diganti'
+    } else {
+        'Tidak perlu'
+    }
+
+?>
